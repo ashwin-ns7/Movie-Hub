3058085b3d4f476c406e78bc7f64a6192b9108bd
ashwin-ns7 <140935149+ashwin-ns7@users.noreply.github.com>
2023-11-26 10:56:59 +0530

    File Explorer
    with add function error

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("inputValue");
+
+//to add movie
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("input").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("movielist").appendChild(li);
+    }
+    document.getElementById("input").value = "";
+   
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
